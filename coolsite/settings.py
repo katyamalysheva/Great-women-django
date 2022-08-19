1de0561c5bc0dd6f8711daf2b5c6cbee04c2fa05
katyamalysheva <katyamalysheva2000@yandex.ru>
2022-08-19 10:58:55 +0300

    change settings

--- coolsite/settings.py
+++ coolsite/settings.py
@@ -89,6 +89,10 @@ DATABASES = {
         'PORT': '',
     }
 }
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 '''DATABASES = {
     'default': {
