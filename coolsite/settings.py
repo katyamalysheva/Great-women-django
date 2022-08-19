b334084574fd87a8e2898843e84b845d462a0575
katyamalysheva <katyamalysheva2000@yandex.ru>
2022-08-19 10:54:41 +0300

    added psycopg2 to db

--- coolsite/settings.py
+++ coolsite/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'coolsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'great_women',
         'USER': 'km',
         'PASSWORD': 'kat',
